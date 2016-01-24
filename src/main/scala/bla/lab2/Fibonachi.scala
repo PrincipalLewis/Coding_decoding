0b2d6dd8216e50ae89f236e1f97e1aa11a0cf8a6
Vados <dflz666@gmail.com>
2016-01-24 19:36:14 +0300

    sad

--- src/main/scala/bla/lab2/Fibonachi.scala
+++ src/main/scala/bla/lab2/Fibonachi.scala
@@ -1,9 +1,7 @@
 package bla.lab2
-import bla.lab2.Factorial
 
 
 class Fibonachi() {
-  val bla = new Factorial()
 
   var fibonachi:List[Int] = List(1,2,3,5,8,13,21,34)
   def undo(x: Int, fib: List[Int], res: Int) : Int = {
@@ -16,6 +14,26 @@ class Fibonachi() {
 
   def un(x: Int) : Int = {
     undo(x, fibonachi, 0)
+  }
+
+  def todo(x: Int, fib: List[Int], res: List[Int], i: Int) : List[Int] = {
+    if (i < 0) {
+      return res.reverse
+    }
+    if (x >= fibonachi(i)) {
+      todo(x-fibonachi(i), fibonachi, 1 :: res, i-1)
+    } else {
+      todo(x, fibonachi, 0 :: res, i-1)
+    }
+  }
+
+  def to(x: Int) : List[Int] = {
+    var i = fibonachi.length-1
+    while (x < fibonachi(i)) {
+      i = i-1
+    }
+
+    todo(x, fibonachi, List(), i)
   }
 }
 
